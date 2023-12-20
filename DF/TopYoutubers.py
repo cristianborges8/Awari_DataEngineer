42632bf5a5ef83832d9764714736607bf0b019f1
cristianborges8 <cristianborges08@gmail.com>
2023-12-19 21:32:23 -0300

    FEAT: Realiza exercicio TOP YOUTUBERS

---
+++ DF/TopYoutubers.py
@@ -0,0 +1,13 @@
+# Tarefa: Top Youtubers
+Esta é a tarefa proposta para a 5ª aula do curso de Engenharia de Dados da Awari. Siga as instruções para desenvolvê-la e entregá-la.
+
+## O que fazer?
+A atividade vai ao encontro do que foi visto em aula, sobre tratamento e limpeza de dados usando Pandas. O passo a passo consiste em:
+
+Fazer download do dataset Top Youtube Artists, do Kaggle. 
+Tratar as colunas sem informação adicionando o texto “Não informado”.
+Formatar a coluna de total de inscritos multiplicando por 100 milhões.
+Formatar coluna “AVG” para 2 casas decimais depois da vírgula.
+Mostrar os top 10 usuários.
+Mostrar primeiros 100 usuários por nome decrescente.
+Salvar um novo CSV com a informação tratada.
--- TopYoutubers.py
+++ TopYoutubers.py
@@ -1,13 +1,27 @@
 # Tarefa: Top Youtubers
-Esta é a tarefa proposta para a 5ª aula do curso de Engenharia de Dados da Awari. Siga as instruções para desenvolvê-la e entregá-la.
+# Fazer download do dataset Top Youtube Artists, do Kaggle. 
+import pandas as pd
+df = pd.read_csv('/Users/cristianborges/Documents/Projetos/Awari_DataEngineer/topyoutube.csv')
 
-## O que fazer?
-A atividade vai ao encontro do que foi visto em aula, sobre tratamento e limpeza de dados usando Pandas. O passo a passo consiste em:
+# Tratar as colunas sem informação adicionando o texto “Não informado”
+df.fillna("Não informado")
 
-Fazer download do dataset Top Youtube Artists, do Kaggle. 
-Tratar as colunas sem informação adicionando o texto “Não informado”.
-Formatar a coluna de total de inscritos multiplicando por 100 milhões.
-Formatar coluna “AVG” para 2 casas decimais depois da vírgula.
-Mostrar os top 10 usuários.
-Mostrar primeiros 100 usuários por nome decrescente.
-Salvar um novo CSV com a informação tratada.
+# Formatar a coluna de total de inscritos multiplicando por 100 milhões
+df = df.rename(columns={'100M': 'Total Subscribers'})
+df['Total Subscribers'] = df['Total Subscribers'] * 100000000
+
+# Formatar coluna “AVG” para 2 casas decimais depois da vírgula.
+df['Avg'] = df['Avg'].round(2)
+
+# Mostrar os top 10 usuários.
+top_10_usuarios = df.head(10)
+print(top_10_usuarios)
+
+
+# Mostrar primeiros 100 usuários por nome decrescente.
+df_sorted = df.sort_values(by='Artist', ascending=False)
+primeiros_100_usuarios = df_sorted.head(100)
+print(primeiros_100_usuarios)
+
+# # Salvar um novo CSV com a informação tratada.
+df.to_csv('topyoutube_.csv', index=False)
